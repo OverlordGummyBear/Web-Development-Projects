31030f2a3561be4c197bafbad9dc583d209ddf20
DANIELSTATIONÆR\danie <danielwlaursen@gmail.com>
2025-10-14 16:48:49 +0200

    Finish section 25 4.3

--- Backend/25 EJS/4.2 Passing Data/index.js
+++ Backend/25 EJS/4.2 Passing Data/index.js
@@ -6,9 +6,22 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  //let firstName = req.body["fName"];
+  //let lastName = req.body["lName"];
+  //let name = firstName + lastName;
+  let name = req.body["fName"].length + req.body["lName"].length;
+
+  const data = {
+    nameLength: name,
+  };
+
+  res.render("index.ejs", data);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
