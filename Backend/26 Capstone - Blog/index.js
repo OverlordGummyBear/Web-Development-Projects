731bc431f13f917b51408f86f5b559298e446fb4
DANIELSTATIONÆR\danie <danielwlaursen@gmail.com>
2025-10-15 14:28:02 +0200

    Adding comments to all files

--- Backend/26 Capstone - Blog/index.js
+++ Backend/26 Capstone - Blog/index.js
@@ -1,47 +1,53 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+//app and port
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true})); // enabling the use of body
+app.use(express.static("public")); //tell index.js where to find the static files e.g. .css / images
 
-const blogArray = [];
+const blogArray = []; //array that saves all posts
 
+//page that asks for users name
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+//when name is entered, go to the blog page - pass along the blogArray and userName
 app.post("/enter", (req, res) => {
     const name = req.body["fName"];
-    console.log(blogArray);
+    //console.log(blogArray);
     res.render("main.ejs", {userName: name, posts: blogArray})
 });
 
-app.post("/create", (req, res) => {
+//when presing create post send user to post creation page
+app.post("/create", (req, res) => { 
     const name = req.body["userName"];
     res.render("create.ejs", {userName: name});
 });
 
-app.post("/submit", (req, res) => {
-    let title = req.body["title"];
-    let description = req.body["description"];;
+//when user saves a post in the post creation page
+app.post("/save", (req, res) => {
     const name = req.body["userName"];
-    let editing = req.body["isEditing"];
-    console.log(req.body);
+    let title = req.body["title"]; //getting the title
+    let description = req.body["description"];; //getting the description
+    let editing = req.body["isEditing"]; //tells whether or not the post was edited or created
+    //console.log(req.body);
 
-    if(editing === "true"){
+    if(editing === "true"){ //checks whether the post saved was edited or created
         blogArray[req.body["id"]] = new blogPost(title, description);
     } else {
-        blogArray.push(new blogPost(title, description));
+        blogArray.push(new blogPost(title, description)); //if not edited, then push the post to the array
     }
 
-    console.log(blogArray)
+    //console.log(blogArray)
 
     res.render("main.ejs", {posts: blogArray, userName: name});
 });
 
+//when clicking on edit send to post creation page and send id so post can be identified
 app.post("/edit", (req, res) => {
     //console.log(req.body);
     const name = req.body["userName"];
@@ -50,18 +56,20 @@ app.post("/edit", (req, res) => {
     res.render("create.ejs", {userName: name, postId: id, posts: blogArray});
 });
 
+//remove post with the index of the delete button that was cliced
 app.post("/delete", (req, res) => {
     const name = req.body["userName"];
     const id = req.body["postId"];
-    blogArray.splice(id, 1);
+    blogArray.splice(id, 1); //remove 1 post at the specific id
 
     res.render("main.ejs", {userName: name, postId: id, posts: blogArray});
 });
 
-app.listen(port, () => {
+app.listen(port, () => { //app is listening at specific port
     console.log(`Listening on port ${port}`);
 })
 
+//blogPost object
 function blogPost(title, description){
     this.title = title;
     this.description = description;
